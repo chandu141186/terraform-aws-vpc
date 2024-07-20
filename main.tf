1ee59cb1b43669dd8a2d98c1a0451afed00515ea
chandu141186 <chandu141186@gmail.com>
2024-07-20 22:31:13 +0530

    aws vpc

--- main.tf
+++ main.tf
@@ -1,74 +1,22 @@
-resource "aws_vpc" "roboshop" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
-}
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.roboshop.id
+module "roboshop" {
+  source = "../vpc"
+  #source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
 
-  tags = {
-    Name = "main"
-  }
-  }
+  # public subnet
+  public_subnets_cidr = var.public_subnets_cidr
 
-  resource "aws_internet_gateway_attachment" "igw" {
-  internet_gateway_id = aws_internet_gateway.igw.id
-  vpc_id              = aws_vpc.roboshop.id
-}
+  # private subnet
+  private_subnets_cidr = var.private_subnets_cidr
 
-resource "aws_subnet" "roboshop_public" {
-  vpc_id     = aws_vpc.roboshop.id
-  cidr_block = "10.0.1.0/24"
+  # database subnet
+  database_subnets_cidr = var.database_subnets_cidr
 
-  tags = {
-    Name = "Main"
-  }
-}
+  #peering
+  is_peering_required = var.is_peering_required
 
-
-
-resource "aws_subnet" "roboshop_public" {
-  vpc_id     = aws_vpc.roboshop.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}
-
-resource "aws_subnet" "roboshop_private" {
-  vpc_id     = aws_vpc.roboshop.id
-  cidr_block = "10.0.2.0/24"
-
-
-}
-
-resource "aws_subnet" "roboshop_database" {
-  vpc_id     = aws_vpc.roboshop.id
-  cidr_block = "10.0.3.0/24"
-}
-
-
-  resource "aws_route_table" "roboshop_public_route" {
-  vpc_id = aws_vpc.roboshop.id
-
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.igw.id
-  }
-
-  
-}
-
-  tags = {
-    Name = "Main"
-  }
 }
